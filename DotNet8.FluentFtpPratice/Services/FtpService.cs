43377b37484b263c9f61458cc056fce5c496de90
Moe Thet Khine <soniapoudel03@gmail.com>
2024-09-05 10:03:52 +0630

    [Added Ftp Service CheckDirectoryExists]

--- DotNet8.FluentFtpPratice/Services/FtpService.cs
+++ DotNet8.FluentFtpPratice/Services/FtpService.cs
@@ -1,6 +1,48 @@
-﻿namespace DotNet8.FluentFtpPratice.Services
+﻿using FluentFTP;
+using System.Net;
+
+namespace DotNet8.FluentFtpPratice.Services
 {
     public class FtpService
     {
+        private readonly IConfiguration _configuration;
+        private readonly string _hostName = string.Empty;
+        private readonly string _userName = string.Empty;
+        private readonly string _password = string.Empty;
+        private readonly AsyncFtpClient _ftp;
+
+        public FtpService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            _hostName = _configuration.GetSection("FtpCredentials")["FtpHostName"]!;
+            _userName = _configuration.GetSection("FtpCredentidals")["FtpUser"]!;
+            _password = _configuration.GetSection("FtpCredentials")["FtpPassword"]!;
+            _ftp = new AsyncFtpClient
+            {
+                Host = _hostName,
+                Credentials = new NetworkCredential(_userName, _password)
+            };
+        }
+
+        public async Task ConnectAsync()
+        {
+            var token = new CancellationToken();
+            await _ftp.Connect(token);
+        }
+
+        public async Task<bool> CheckDirectoryExistsAsync(string directory)
+        {
+            try
+            {
+                var token = new CancellationToken ();
+                await _ftp.Connect(token);
+                return await _ftp.DirectoryExists(directory, token);
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+                throw;
+            }
+        }
     }
 }
