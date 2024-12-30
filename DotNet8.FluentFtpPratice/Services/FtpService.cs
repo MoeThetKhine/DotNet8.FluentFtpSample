e925d252d809d382fd543583d21cf0086d463337
Moe Thet Khine <soniapoudel03@gmail.com>
2024-12-31 00:08:28 +0630

    Feat : Add Connect Async

--- DotNet8.FluentFtpPratice/Services/FtpService.cs
+++ DotNet8.FluentFtpPratice/Services/FtpService.cs
@@ -15,7 +15,7 @@ namespace DotNet8.FluentFtpPratice.Services
         {
             _configuration = configuration;
             _hostName = _configuration.GetSection("FtpCredentials")["FtpHostName"]!;
-            _userName = _configuration.GetSection("FtpCredentidals")["FtpUser"]!;
+            _userName = _configuration.GetSection("FtpCredentials")["FtpUserName"]!;
             _password = _configuration.GetSection("FtpCredentials")["FtpPassword"]!;
             _ftp = new AsyncFtpClient
             {
@@ -30,99 +30,6 @@ namespace DotNet8.FluentFtpPratice.Services
             await _ftp.Connect(token);
         }
 
-        public async Task<bool> CheckDirectoryExistsAsync(string directory)
-        {
-            try
-            {
-                var token = new CancellationToken ();
-                await _ftp.Connect(token);
-                return await _ftp.DirectoryExists(directory, token);
-            }
-            catch(Exception ex)
-            {
-                Console.WriteLine(ex.ToString());
-                throw;
-            }
-        }
-        public async Task<bool> CreateDirectoryAsync(string directory)
-        {
-            try
-            {
-                var token = new CancellationToken();
-                await _ftp.Connect(token);
-
-                return await _ftp.CreateDirectory(directory,true,token);
-            }
-            catch(Exception ex)
-            {
-                if(ex.InnerException is not null)
-                {
-                    Console.WriteLine(ex.InnerException);
-                }
-                Console.WriteLine(ex.ToString());
-                throw;
-            }
-        }
-        public async Task UploadFileAsync(IFormFile file, string directory)
-        {
-            var tempFilePath = Path.GetTempFileName();
-
-            try
-            {
-                using (var stream = new FileStream(tempFilePath, FileMode.Create))
-                {
-                    await file.CopyToAsync(stream);
-                }
-                var token = new CancellationToken();
-                await _ftp.Connect(token);
-                Console.WriteLine("Connected to FTP server.");
-
-                await _ftp.CreateDirectory(directory,token);
-                Console.WriteLine($"Remote directory'{directory}' checked/created.");
-
-                var remoteFilePath = Path.Combine(directory,file.FileName).Replace("\\","/");
-
-                var success = await _ftp.UploadFile(tempFilePath, remoteFilePath,token:token);
-
-                Console.WriteLine("File Uploaded Successfully!");
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine($"Exception:{ex.Message}");
-                if(ex.InnerException != null)
-                {
-                    Console.WriteLine($"Inner Exception:{ex.InnerException.Message}");
-                }
-                throw;
-            }
-            finally
-            {
-                if (File.Exists(tempFilePath))
-                {
-                    File.Delete(tempFilePath);
-                    Console.WriteLine("Temporary file deleted.");
-                }
-            }
-        }
-        public async Task DeleteFileAsync(string filePath)
-        {
-            try
-            {
-                var token = new CancellationToken();
-                await _ftp.Connect(token);
-
-                await _ftp.DeleteFile(filePath);
-                Console.WriteLine("File Deleted Successfully!");
-            }
-            catch(Exception ex)
-            {
-
-                if(ex.InnerException is not null)
-                {
-                    Console.WriteLine(ex.InnerException.Message);
-                }
-                Console.WriteLine(ex.Message);
-            }
-        }
+      
     }
 }
